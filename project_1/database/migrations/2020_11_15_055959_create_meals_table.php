b45e9c384b421c9bd7b4379242221f62bd63ae01
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-11-15 16:54:12 +0600

    Create 2020_11_15_055959_create_meals_table.php

---
+++ project_1/database/migrations/2020_11_15_055959_create_meals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->date('date');
+            $table->boolean('breakfast');
+            $table->boolean('lunch');
+            $table->boolean('dinner');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+}
